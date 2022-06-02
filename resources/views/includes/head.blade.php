3c0259ea43f6138e7a4e527d1968b326a79145ec
06066060606060 <xmicky+git@hotmail.fr>
2022-06-02 17:32:47 +0400

    local1

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,5 @@
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+       <title>Laravel</title>
+       <script src="{{ mix('js/app.js') }}" defer></script>
+       <link href="{{ asset('css/app.css') }}" rel="stylesheet">
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+
+<html>
+<head>
+    @include('includes.head')
+</head>
+
+<body>
+
+    <header>
+        @include('includes.header')
+    </header>
+
+    <main>
+        @yield('main')
+    </main>
+
+    <main>
+        @yield('content')
+    </main>
+    
+    <footer>
+        @include('includes.footer')
+    </footer>
+
+
+</body>
+</html>
