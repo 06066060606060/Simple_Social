3c945ed930a511e5322e4f91f878a4d87a40c7ed
equixor romain <r.equixor@gmail.com>
2022-06-06 03:25:50 +0400

    test ajout com

--- resources/views/includes/profiletest.blade.php
+++ resources/views/includes/profiletest.blade.php
@@ -1,69 +1,105 @@
+{{-- w-2/3  border border-gray-200 --}}
+<div class=" p-4  ">
 
-    
-            <div class="p-4">
-
-                <article class="px-4 py-24 mx-auto max-w-7xl" itemid="#" itemscope itemtype="http://schema.org/BlogPosting">
-                    <div class="w-full mx-auto mb-10 text-left md:w-3/4 lg:w-1/2">
-                        <div class="pb-6 mb-6 border-b border-gray-200">
-                            <h1 class="mb-3 text-3xl font-bold text-gray-900 md:leading-tight md:text-4xl"
-                                itemprop="headline" title="Rise of Tailwind - A Utility First CSS Framework">
-                                no matter what goes down
-                            </h1>
-                            <p class="text-base text-gray-500">January 18, 2021 — Ecrit par jok pololo</p>
-                        </div>
-
-                        <img src="./img/logo.png" class="w-full h-64 bg-center rounded " alt="Kutty" />
-                    </div>
-
-
-                    <div class="w-full mx-auto prose md:w-3/4 lg:w-1/2">
-                        <p class="my-4">
-                            [Verse 2: Ski Mask the Slump God]
-                            I been seein’ you in designer lately (Yuh)
-                            No surprise the Devil Wears Prada, baby (Okay)
-                            Feelin’ like I’m drownin’ in some lava lately (Yuh)
-                            I taught you some game and you still tryna play me (Ha-ha, okay)
-                            Gotta tell myself, “Keep your cool (Huh, yuh, huh), simmer down” (Huh, yuh)
-                            They say shit that go around will come back around (Huh, huh, huh)
-                            Try to keep my tool, huh, like a mechanic now (Huh, huh yuh)
-                            No matter what goes down
-                        </p>
-                        <p class="my-4 page">[Chorus: Oliver Tree]
-                            Lifе goes on and on and on and on and on and
-                            On and on and on
-                            On and on and on and on and on and
-                            On and on and on
-                            On and on and on and on and on and
-                            On and on and on
-                            On and on and on and on and on and
-                            On and on and on</p>
-                        <p>...</p>
-                    </div>
-                    <div class="flex flex-col w-full mx-auto mb-6 space-x-2 text-left md:w-3/4 lg:w-1/2">
-                        <input class="w-full rounded" type="text" placeholder="entrez votre commentaire ">
-                    </div>
-                    <div class="flex items-center justify-end w-full mx-auto mb-6 space-x-2 text-left md:w-3/4 lg:w-1/2">
-                        <i class="mr-3 fa-solid fa-thumbs-up "></i>
-                        <i class="page fa-solid fa-thumbs-down "></i>
-                        <i class="fa-solid page fa-share "></i>
-                        <i class="fa-solid page fa-ellipsis"></i>
-                    </div>
-
-                    <div class="flex items-center w-full mx-auto mb-6 space-x-2 text-left md:w-3/4 lg:w-1/2">
-                        <p class="text-gray-600">Partager l'article </p>
-                        <a href="#" class="text-gray-600 hover:text-gray-900">
-                            <i class="fa-brands fa-instagram"></i>
+    <article class="px-4  py-24 mx-auto max-w-7xl" itemid="#" itemscope itemtype="http://schema.org/BlogPosting">
+        <div class="w-full mx-auto mb-10 text-left md:w-3/4 lg:w-1/2">
+            <div class="pb-6 mb-6 border-b border-gray-200">
+                <div class="flex items-center my-4 border-t border-gray-200">
+                    <a href="#" class=" avatar avatar-sm">
+                        <img src="./img/Ellipse2.png" alt="Photo of Praveen Juge" />
+                    </a>
+                    <div>
+                        <a href="">
+                            <p class="text-base font-bold text-black lg: ml-3  "> jok pololo </p>
                         </a>
-                        <a href="#" class="text-gray-600 hover:text-gray-900">
-                            <i class="fa-brands fa-facebook"></i>
-                        </a>
-                        <a href="#" class="text-gray-600 hover:text-gray-900">
-                            <i class="fa-brands fa-linkedin"></i>
-                        </a>
+                        <p class="text-base  text-gray-500 lg:   "> Junary 18, 2021</p>
                     </div>
+                </div>
 
-                </article>
+                <h1 class="mb-3 text-3xl font-bold text-gray-900 md:leading-tight md:text-4xl" itemprop="headline"
+                    title="Rise of Tailwind - A Utility First CSS Framework">
+                    no matter what goes down
+                </h1>
+
+
             </div>
 
-       
+            <img src="./img/logo.png" class="w-full h-64 bg-center rounded " alt="Kutty" />
+        </div>
 
+
+        <div class="w-full mx-auto prose md:w-3/4 lg:w-1/2">
+            <p class="my-4">
+                [Verse 2: Ski Mask the Slump God]
+                I been seein’ you in designer lately (Yuh)
+                No surprise the Devil Wears Prada, baby (Okay)
+                Feelin’ like I’m drownin’ in some lava lately (Yuh)
+                I taught you some game and you still tryna play me (Ha-ha, okay)
+                Gotta tell myself, “Keep your cool (Huh, yuh, huh), simmer down” (Huh, yuh)
+                They say shit that go around will come back around (Huh, huh, huh)
+                Try to keep my tool, huh, like a mechanic now (Huh, huh yuh)
+                No matter what goes down
+            </p>
+            <p class="my-4 page">[Chorus: Oliver Tree]
+                Lifе goes on and on and on and on and on and
+                On and on and on
+                On and on and on and on and on and
+                On and on and on
+                On and on and on and on and on and
+                On and on and on
+                On and on and on and on and on and
+                On and on and on</p>
+            <p>...</p>
+        </div>
+        
+        <div class="flex  my-4  w-full mx-auto mb-6 space-x-2  md:w-3/4 lg:w-1/2">
+
+            <a href="#" class=" avatar avatar-sm">
+                <img src="./img/Ellipse2.png" alt="Photo of Praveen Juge" />
+            </a>
+
+            <input class="w-full rounded-lg placeholder:text-white  bg-slate-300 border-0 form-input" type="text" placeholder="Ecrivez un commentaire ">
+       
+        </div>
+
+        <div class="flex border-y border-gray-200  w-full mx-auto mb-6 space-x-2 text-left md:w-3/4 lg:w-1/2">
+
+            <a href="#" class=" avatar avatar-sm">
+                <img src="./img/Ellipse2.png" alt="Photo of Praveen Juge" />
+            </a>
+            <a href="">
+                <p class="text-base font-bold text-black lg: ml-3  "> jok pololo </p>
+            </a>
+            <div>
+
+            </div>
+
+        </div>
+
+        <div class="flex flex-col w-full mx-auto mb-6 space-x-2 text-left md:w-3/4 lg:w-1/2">
+            <div class=" -ml-3 form-icon md:block ">
+                <input class="w-full rounded  border-0 form-input" type="text" placeholder="Ecrivez un commentaire ">
+            </div>
+        </div>
+        <div class="flex items-center justify-end w-full mx-auto mb-6 space-x-2 text-left md:w-3/4 lg:w-1/2">
+            <i class="mr-3 fa-solid fa-thumbs-up "></i>
+            <i class="page fa-solid fa-thumbs-down "></i>
+            <i class="fa-solid page fa-share "></i>
+            <i class="fa-solid page fa-ellipsis"></i>
+        </div>
+
+        <div class="flex items-center w-full mx-auto mb-6 space-x-2 text-left md:w-3/4 lg:w-1/2">
+            <p class="text-gray-600">Partager l'article </p>
+            <a href="#" class="text-gray-600 hover:text-gray-900">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+            <a href="#" class="text-gray-600 hover:text-gray-900">
+                <i class="fa-brands fa-facebook"></i>
+            </a>
+            <a href="#" class="text-gray-600 hover:text-gray-900">
+                <i class="fa-brands fa-linkedin"></i>
+            </a>
+        </div>
+
+    </article>
+</div>
