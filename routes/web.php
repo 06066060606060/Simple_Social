7944a29c0c7d1d05f1156ff2a7b737a5422f2680
equixor romain <r.equixor@gmail.com>
2022-06-08 13:33:50 +0400

    s

--- routes/web.php
+++ routes/web.php
@@ -30,17 +30,8 @@ Route::post('/authenticate', [AuthController::class, 'authenticate'])->name('aut
 
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-<<<<<<< HEAD
-Route::get('/', [AuthController::class, 'boucleBackend']);
-
-Route::delete('/delete/{id}', [AuthController::class, 'delete'])->whereNumber('id')->name('delete');
-
-Route::post('/update/{id}', [AuthController::class, 'update'])->whereNumber('id')->name('update');
-
-=======
 Route::get('/', [Controller::class, 'boucleBackend']);
 
 Route::delete('/delete/{id}', [Controller::class, 'delete'])->whereNumber('id')->name('delete');
 
 Route::post('/update/{id}', [Controller::class, 'update'])->whereNumber('id')->name('update');
->>>>>>> 7f4987fc1a856e823e98ae3f4b11564985f8841f
