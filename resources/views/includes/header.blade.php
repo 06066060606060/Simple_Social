a13007e7638dc63630b5f5c505dd38cedf2b6a47
06066060606060 <xmicky+git@hotmail.fr>
2022-06-08 09:13:19 +0400

    commit mercredi matin

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,11 +3,11 @@
            <img src="/img/logo.png" alt="Logo" />
        </div>
    </div>
-   <section class="min-h-screen bg-gray-200" x-data="{ sideBar: false, settings: false, mur: true }">
-       <nav class="z-20 h-screen pb-10 overflow-x-hidden overflow-y-auto transition origin-left transform bg-gray-900 w-60 md:translate-x-0 fixed top-48"
+   <section class="min-h-screen bg-gray-200" x-data="{ sideBar: false, settings: false, mur: true, profils: false }">
+       <nav class="fixed z-20 h-screen pb-10 overflow-x-hidden overflow-y-auto transition origin-left transform bg-gray-900 w-60 md:translate-x-0 top-48"
            :class="{ '-translate-x-full': !sideBar, 'translate-x-0': sideBar }" @click.away="sideBar = false">
 
-           <nav class="text-gray-500 pt-8" aria-label="Main Navigation">
+           <nav class="pt-8 text-gray-500" aria-label="Main Navigation">
 
                <a class="flex items-center px-4 py-3 transition cursor-pointer group hover:bg-gray-800 hover:text-gray-200"
                    href="#" @click="settings = false, mur = true">
