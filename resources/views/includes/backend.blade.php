2b7e28f78a1ca9d614f5dd7923eeb9535e076b52
06066060606060 <xmicky+git@hotmail.fr>
2022-06-10 09:23:30 +0400

    FIX

--- resources/views/includes/backend.blade.php
+++ resources/views/includes/backend.blade.php
@@ -55,25 +55,8 @@
 
                 </div>
             </div>
-<<<<<<< HEAD
 
         
-=======
-            <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-            <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-            <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
-            <script>
-                $(document).ready(function() {
-
-                    var table = $('#example').DataTable({
-                            responsive: true
-                        })
-                        .columns.adjust()
-                        .responsive.recalc();
-                });
-            </script>
-
->>>>>>> a60086b35ae15ec3c7d8cae7898707751fb15399
         </div>
         <div x-cloak x-show.transition.in.opacity.duration.750ms="selected === 'option-2'" class="p-4">
             <div class="container w-full">
