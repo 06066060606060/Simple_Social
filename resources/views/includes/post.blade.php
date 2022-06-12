ac368b7e0f54bc23532513051501f5689f9216e0
06066060606060 <xmicky+git@hotmail.fr>
2022-06-13 01:10:57 +0400

    fix

--- resources/views/includes/post.blade.php
+++ resources/views/includes/post.blade.php
@@ -48,7 +48,7 @@
             <input type="hidden" name="post_id" value="{{ $post->id }}">
             <input type="hidden" name="user_id" value="{{Auth::user()->id}}">
             <div class="flex flex-row w-full mx-auto my-2 space-x-2 md:w-3/4">
-            <img src="{{ Storage::url(Auth::user()->photo) }}" class="w-12 h-12 pt-1 rounded-full" />
+            <img src="{{ Storage::url(Auth::user()->photo) }}" class="w-12 h-12 rounded-full" />
                 <input name="comm" class="w-full ml-2 border-0 rounded-lg placeholder:text-gray-800 bg-slate-300 form-input" type="text"
                 placeholder="Laisser un commentaire">
         </div>
