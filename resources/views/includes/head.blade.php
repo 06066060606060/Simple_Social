67877e94b7eb81b95bb5b803eb49196f72c19bba
Shift <shift@laravelshift.com>
2022-07-04 18:18:43 +0000

    Use new `@vite` Blade directive

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,5 +1,5 @@
        <meta charset="utf-8">
        <meta name="viewport" content="width=device-width, initial-scale=1">
        <title>Laravel</title>
-       <script src="{{ mix('js/app.js') }}" defer></script>
+       @vite('resources/js/app.js')
        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
